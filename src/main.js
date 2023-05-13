d85ebb0500b0c17bbe7dad6c35d9f0f4a632f875
Grayson Richard <graysonrichard@Graysons-MacBook-Pro.local>
2023-05-12 20:08:06 -0600

    reformating and removing button, planning to add a counter for how many days

--- src/main.js
+++ src/main.js
@@ -18,14 +18,10 @@ function activateCountdown(element, dateString)
     }, 250);
 }
 
-activateCountdown(document.getElementById("myCountdown"), "2023-8-5");
-
 var i = 0;
 function move(startDate, endDate) {
     const start = dayjs(startDate);
     const end = dayjs(endDate);
-    console.log("end = " + end);
-    console.log("start = " + start) ;
 // each iteration of the interval i need to calc the difference
 // in seconds or maybe days between the start and end
   if (i == 0) {
@@ -50,4 +46,49 @@ function move(startDate, endDate) {
   }
 }
 
-move("2023-4-20", "2023-8-5")
+activateCountdown(document.getElementById("myCountdown"), "2023-8-5");
+
+move("2023-5-10", "2023-8-5")
+
+
+
+
+// buttons prefrences
+// and module
+
+// Get the modal
+// var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+// var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
+
+// When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
+
+// When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
+
+var date = document.getElementById("date");
+
+if(date == "Date(YYYY-M-D):")
+{
+    activateCountdown(document.getElementById("myCountdown"), date);
+
+    move("2023-5-10", date)
+}
+
+// YOU SHOULD ADD A COUNTER THAT STATES HOW MANY DAYS HAVE PASSED ALREADY!!
