b8d0f881876cedba23343ad3f7a528753fda1aef
Grayson Richard <graydragon11@gmail.com>
2023-09-13 12:03:45 -0600

    working to merge

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 dayjs.extend(dayjs_plugin_duration);
 
-function activateCountdown(element, dateString)
+function activateCountdown(element, dateString, startDate) // the start date is so that the days past can be updated
 {
+    const start = dayjs(startDate);
+
     const targetDate = dayjs(dateString);
 
     element.querySelector(".until__event").textContent = `Until ${targetDate.format("D MMMM YYYY")}`
@@ -10,18 +12,29 @@ function activateCountdown(element, dateString)
         // calc difference in time
         const now = dayjs();
         const duration = dayjs.duration(targetDate.diff(now));
+        const timePast = dayjs.duration(now.diff(start));
 
         element.querySelector(".until__numeric--seconds").textContent = duration.seconds().toString().padStart(2, "0");
         element.querySelector(".until__numeric--minutes").textContent = duration.minutes().toString().padStart(2, "0");
         element.querySelector(".until__numeric--hours").textContent = duration.hours().toString().padStart(2, "0");
         element.querySelector(".until__numeric--days").textContent = duration.asDays().toFixed(0).toString().padStart(2, "0");
+        
+        // console.log( timePast.asDays().toFixed(0).toString().padStart(2, "0"))
+        // element.querySelector(".until__numeric--dayz").textContent = 10;
+        element.querySelector(".until__numeric--secondz").textContent = timePast.seconds().toString().padStart(2, "0");
+        element.querySelector(".until__numeric--minutez").textContent = timePast.minutes().toString().padStart(2, "0");
+        element.querySelector(".until__numeric--hourz").textContent = timePast.hours().toString().padStart(2, "0");
+        element.querySelector(".until__numeric--dayz").textContent = timePast.asDays().toFixed(0).toString().padStart(2, "0");
+       
+
+        
     }, 250);
 }
 
 var i = 0;
-function move(startDate, endDate) {
-    const start = dayjs(startDate);
-    const end = dayjs(endDate);
+function move(startDate, endDate, element) {
+  const start = dayjs(startDate);
+  const end = dayjs(endDate);
 // each iteration of the interval i need to calc the difference
 // in seconds or maybe days between the start and end
   if (i == 0) {
@@ -41,14 +54,16 @@ function move(startDate, endDate) {
         const percent = (difference / total)*100;
         width = percent;
         elem.style.width = width + "%";
+        element.querySelector("until__component--percent") = width;
+        console.log(width)
       }
     }
   }
 }
 
-activateCountdown(document.getElementById("myCountdown"), "2023-8-5");
+activateCountdown(document.getElementById("myCountdown"), "2023-8-1", "2023-5-10");
 
-move("2023-5-10", "2023-8-5")
+move("2023-5-10", "2023-8-1")
 
 
 
@@ -86,7 +101,7 @@ var date = document.getElementById("date");
 
 if(date == "Date(YYYY-M-D):")
 {
-    activateCountdown(document.getElementById("myCountdown"), date);
+    activateCountdown(document.getElementById("myCountdown"), date, "2023-5-10");
 
     move("2023-5-10", date)
 }
